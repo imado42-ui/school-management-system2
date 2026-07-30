eee001cfb1f92d18b85003c143d902a98b5176a0
imado42-ui <saadiimad4242@gmail.com>
2026-07-31 00:55:22 +0100

    Update delete_timetable.php

--- modules/timetable/delete_timetable.php
+++ modules/timetable/delete_timetable.php
@@ -1,1 +1,16 @@
-..
+<?php
+require_once "../../config/database.php";
+require_once "../../includes/auth.php";
+
+if (!isset($_GET['id'])) {
+    header("Location: timetable.php");
+    exit;
+}
+
+$id = (int) $_GET['id'];
+
+$stmt = $pdo->prepare("DELETE FROM timetable WHERE id = ?");
+$stmt->execute([$id]);
+
+header("Location: timetable.php");
+exit;
