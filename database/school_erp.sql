70679cb9e03c536fb3d9f1d485a8f9fd35a02257
imado42-ui <saadiimad4242@gmail.com>
2026-07-30 14:04:02 +0100

    Update school_erp.sql

--- database/school_erp.sql
+++ database/school_erp.sql
@@ -6,3 +6,14 @@ CREATE TABLE users (
     role ENUM('admin','teacher') DEFAULT 'admin',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    gender ENUM('ذكر','أنثى'),
+    birthdate DATE,
+    class VARCHAR(50),
+    phone VARCHAR(20),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
