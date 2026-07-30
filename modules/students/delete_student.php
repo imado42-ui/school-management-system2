790829c17c7f62c09db14e34953da04ef1387a8a
imado42-ui <saadiimad4242@gmail.com>
2026-07-30 14:18:51 +0100

    Update delete_student.php

--- modules/students/delete_student.php
+++ modules/students/delete_student.php
@@ -2,13 +2,15 @@
 require_once "../../config/database.php";
 require_once "../../includes/auth.php";
 
-if(isset($_GET['id'])){
-
-    $id = $_GET['id'];
-
-    $stmt = $pdo->prepare("DELETE FROM students WHERE id=?");
-    $stmt->execute([$id]);
+if (!isset($_GET['id'])) {
+    die("رقم التلميذ غير موجود.");
 }
+
+$id = intval($_GET['id']);
+
+$stmt = $pdo->prepare("DELETE FROM students WHERE id = ?");
+$stmt->execute([$id]);
 
 header("Location: students.php");
 exit;
+?>
