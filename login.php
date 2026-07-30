f742cee043b964bd92e7e10322199846d46f97a6
imado42-ui <saadiimad4242@gmail.com>
2026-07-30 13:55:22 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,66 +1,26 @@
 <?php
 session_start();
-require_once "config/database.php";
 
-if(isset($_SESSION['user_id'])){
+if(isset($_SESSION['user'])){
     header("Location: dashboard.php");
-    exit();
+    exit;
 }
 
-$error = "";
-
-if(isset($_POST['login'])){
-
-    $username = trim($_POST['username']);
-    $password = trim($_POST['password']);
-
-    if(empty($username) || empty($password)){
-        $error = "يرجى إدخال اسم المستخدم وكلمة المرور.";
-    }else{
-
-        $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
-        $stmt->execute([$username]);
-        $user = $stmt->fetch();
-
-        if($user && password_verify($password,$user['password'])){
-
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['username'] = $user['username'];
-
-            header("Location: dashboard.php");
-            exit();
-
-        }else{
-            $error = "بيانات الدخول غير صحيحة.";
-        }
-    }
-}
+include "includes/header.php";
 ?>
 
-<?php include "includes/header.php"; ?>
+<h2>تسجيل الدخول</h2>
 
-<div class="login-box">
+<form action="index.php" method="POST">
+    <label>اسم المستخدم</label><br>
+    <input type="text" name="username" required><br><br>
 
-    <h2>تسجيل الدخول</h2>
+    <label>كلمة المرور</label><br>
+    <input type="password" name="password" required><br><br>
 
-    <?php if($error!=""){ ?>
-        <p style="color:red;"><?php echo $error; ?></p>
-    <?php } ?>
+    <button type="submit">دخول</button>
+</form>
 
-    <form method="post">
-
-        <input type="text" name="username" placeholder="اسم المستخدم" required>
-
-        <br><br>
-
-        <input type="password" name="password" placeholder="كلمة المرور" required>
-
-        <br><br>
-
-        <button type="submit" name="login">دخول</button>
-
-    </form>
-
-</div>
-
-<?php include "includes/footer.php"; ?>
+<?php
+include "includes/footer.php";
+?>
