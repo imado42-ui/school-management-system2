6658a6d820a01a705066efc584cda09719666043
imado42-ui <saadiimad4242@gmail.com>
2026-07-30 13:54:41 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,33 +1,21 @@
 <?php
-session_start();
-
-if(!isset($_SESSION['user_id'])){
-    header("Location: login.php");
-    exit();
-}
-
+include "includes/auth.php";
 include "includes/header.php";
-include "includes/sidebar.php";
 ?>
 
-<div style="margin-left:270px;padding:20px;">
+<h2>لوحة التحكم</h2>
 
-    <h2>لوحة التحكم</h2>
-
-    <p>مرحباً <?php echo $_SESSION['username']; ?></p>
-
-    <hr>
-
-    <h3>إحصائيات النظام</h3>
-
-    <ul>
-        <li>👨‍🎓 عدد الطلاب</li>
-        <li>👨‍🏫 عدد الأساتذة</li>
-        <li>🏫 عدد الأقسام</li>
-        <li>📚 عدد المواد</li>
-    </ul>
-
-</div>
+<ul>
+    <li><a href="modules/students/students.php">إدارة الطلاب</a></li>
+    <li><a href="modules/teachers/teachers.php">إدارة الأساتذة</a></li>
+    <li><a href="modules/classes/classes.php">إدارة الأقسام</a></li>
+    <li><a href="modules/subjects/subjects.php">إدارة المواد</a></li>
+    <li><a href="modules/attendance/attendance.php">إدارة الحضور</a></li>
+    <li><a href="modules/grades/grades.php">إدارة النقاط</a></li>
+    <li><a href="modules/exams/exams.php">إدارة الامتحانات</a></li>
+    <li><a href="modules/reports/reports.php">التقارير</a></li>
+    <li><a href="modules/users/users.php">إدارة المستخدمين</a></li>
+</ul>
 
 <?php
 include "includes/footer.php";
