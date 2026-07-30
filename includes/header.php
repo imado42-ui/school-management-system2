d280518ff815887c06f5a39a8eac1d390af09a67
imado42-ui <saadiimad4242@gmail.com>
2026-07-30 01:04:44 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="ar" dir="rtl">
+<html lang="ar">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>نظام تسيير المدارس</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School ERP</title>
 
-<link rel="stylesheet" href="../assets/style.css">
-
+    <link rel="stylesheet" href="/assets/css/style.css">
 </head>
+
 <body>
+
+<header>
+    <h1>School Management System</h1>
+</header>
