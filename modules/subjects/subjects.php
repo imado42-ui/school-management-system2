455e01c3fbc7832acb3f8bac8c1ec22ab2f504ec
imado42-ui <saadiimad4242@gmail.com>
2026-07-30 14:33:42 +0100

    Update subjects.php

--- modules/subjects/subjects.php
+++ modules/subjects/subjects.php
@@ -1,1 +1,52 @@
-..
+<?php
+require_once "../../config/database.php";
+require_once "../../includes/auth.php";
+require_once "../../includes/header.php";
+
+$stmt = $pdo->query("SELECT * FROM subjects ORDER BY id DESC");
+$subjects = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<h2>إدارة المواد الدراسية</h2>
+
+<p>
+    <a href="add_subject.php">➕ إضافة مادة</a>
+</p>
+
+<table border="1" cellpadding="8" cellspacing="0">
+
+<tr>
+    <th>#</th>
+    <th>اسم المادة</th>
+    <th>المعامل</th>
+    <th>الإجراءات</th>
+</tr>
+
+<?php foreach($subjects as $subject): ?>
+
+<tr>
+
+<td><?= $subject['id']; ?></td>
+
+<td><?= $subject['subject_name']; ?></td>
+
+<td><?= $subject['coefficient']; ?></td>
+
+<td>
+<a href="edit_subject.php?id=<?= $subject['id']; ?>">✏️ تعديل</a>
+|
+<a href="delete_subject.php?id=<?= $subject['id']; ?>"
+onclick="return confirm('هل تريد حذف المادة؟');">
+🗑 حذف
+</a>
+</td>
+
+</tr>
+
+<?php endforeach; ?>
+
+</table>
+
+<?php
+require_once "../../includes/footer.php";
+?>
